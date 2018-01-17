ac2825e87a32a815265703b4941adfe5e634f334
cepthomas <cepthomas@gmail.com>
2018-01-17 17:09:40 -0500

    WIP

--- Common/Properties/AssemblyInfo.cs
+++ Common/Properties/AssemblyInfo.cs
@@ -2,35 +2,9 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("Common")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("Nebulator.Common")]
+[assembly: AssemblyDescription("Nebulator Common")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft")]
-[assembly: AssemblyProduct("Common")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2018")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
+[assembly: AssemblyProduct("Nebulator")]
 
-// Setting ComVisible to false makes the types in this assembly not visible
-// to COM components.  If you need to access a type in this assembly from
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("ff958faf-e474-4e58-b787-f166d9cac80f")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+// Version information is supplied by CommonAssemblyInfo.cs.
---
+++ CommonAssemblyInfo.cs
@@ -0,0 +1,22 @@
+using System.Reflection;
+
+// This file comprises the common parts of the AssemblyInfo.cs for the Nebulator assemblies.
+// The main purpose is to ensure all generated assemblies have the same major/minor version
+// number, which can be set manually below prior to building. Note that setting the * forces
+// the Build number to be the number of days since Dec 31, 1999 and the Revision number is
+// the number of seconds since midnight, divided by 2.
+// Suppressing AssemblyFileVersion causes it to take the same value as AssemblyVersion.
+
+// To use it in your project, remove these lines from your AssemblyInfo.cs:
+[assembly: AssemblyCompany("Tyrell Corp")]
+[assembly: AssemblyCopyright("Me")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: AssemblyVersion("0.9.*")]
+
+// Add a note: Version information is supplied by CommonAssemblyInfo.cs.
+
+// Next, you need to link this common file into your projects right beside your edited AssemblyInfo.cs file.
+// To do this, right click on your project and choose Add | Existing Item from the context menu.
+// Navigate to this file but instead of clicking Add, click the little down-arrow 
+// next to add and choose "Add as Link".
